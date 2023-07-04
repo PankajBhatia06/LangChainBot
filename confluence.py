719229d753627eee373f7aa82bded472e18eaf7e
Pankaj Bhatia <pjbhatia100@gmail.com>
2023-07-04 13:34:53 +0530

    Initial commit

---
+++ confluence.py
@@ -0,0 +1,7 @@
+from langchain.document_loaders import ConfluenceLoader
+
+loader = ConfluenceLoader(url="", token="")
+documents = loader.load(
+    space_key="", include_attachments=True, limit=50, max_pages=50)
+
+st.write(documents)
