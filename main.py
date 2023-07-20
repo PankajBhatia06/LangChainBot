7b8f1f955035790aab92660d72add4e65d76f0fd
Mohammed Saif <Mohammed.Saif@T-BX.COM>
2023-07-20 08:56:19 +0100

    Post and get info

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
   
   
-@app.route('/hello', methods=['GET'])
+@app.route('/handleget', methods=['GET'])
 def helloworld():
     if(request.method == 'GET'):
         data = {"data": "Hello World"}
         return jsonify(data)
   
+@app.route('/handlepost', methods=['POST'])
+def handle_post():
+    if (request.method == 'POST'):
+        textData = request.form['testData']
+        data = {"response" : textData}
+        return jsonify(data)
   
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=3445)
